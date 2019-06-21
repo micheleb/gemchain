106db9641bc4ae6a67140e2468ade7dd3bbf11e9
Michele Bonazza <michele@sketchtogether.com>
2019-06-21 11:49:54 +0200

    Add colors and animation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,65 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
-import Game from "../Game/Game";
+import React, { useRef, useState } from "react";
+import { Button, Col, Form, FormControlProps, Row } from "react-bootstrap";
+import Game, { cols, GameRef } from "../Game/Game";
 import "./App.css";
 
-const App: React.FC = () => (
-  <div className="App">
-    <Row className="App-header">
-      <Col md={12}>
-        Gemchain{" "}
-        <span role="img" aria-label="gem emoji">
-          💎
-        </span>
-      </Col>
-    </Row>
-    <Row>
-      <Col md={{ span: 10, offset: 1 }} className="GameContainer">
-        <Game />
-      </Col>
-    </Row>
-  </div>
-);
+const colors = ["red", "blue", "green", "yellow", "purple"];
+
+const App: React.FC = () => {
+  const gameRef = useRef<GameRef>({ addGem: () => null });
+
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
+  const [selectedCol, setSelectedCol] = useState("0");
+
+  const onColorChange = (e: React.FormEvent<FormControlProps>) =>
+    setSelectedColor(e.currentTarget.value as string);
+
+  const onSelectionChange = (e: React.FormEvent<FormControlProps>) =>
+    setSelectedCol(e.currentTarget.value as string);
+
+  const addGem = () =>
+    gameRef.current!.addGem({ color: selectedColor, row: 0, col: parseInt(selectedCol, 10) });
+
+  return (
+    <div className="App">
+      <Row className="App-header">
+        <Col md={12}>
+          Gemchain{" "}
+          <span role="img" aria-label="gem emoji">
+            💎
+          </span>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }} className="GameContainer">
+          <Game ref={gameRef} />
+        </Col>
+        <Col md={2}>
+          <Form>
+            <Form.Group>
+              <Form.Label>Gem color</Form.Label>
+              <Form.Control as="select" onChange={onColorChange} value={selectedColor}>
+                {colors.map((color) => (
+                  <option key={`opt-${color}`}>{color}</option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Add gem to column</Form.Label>
+              <Form.Control as="select" onChange={onSelectionChange} value={selectedCol}>
+                {cols.map((_, i) => (
+                  <option key={`opt-${i}`}>{i}</option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Button variant="secondary" onClick={addGem}>
+              Add
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,23 +1,86 @@
-import React from "react";
+import React, { forwardRef, useImperativeHandle, useState } from "react";
+import { useInterval } from "../../effects";
 import "./Game.css";
 
-const cols = Array(6).fill("");
-const rows = Array(13).fill("");
+// as in some other rather similar game
+const ROW_COUNT = 13;
+const COL_COUNT = 6;
 
-const Game = () => {
-  const getCol = (r: number, c: number) => (
-    <span key={`${r}-${c}`} className="Cell">
-      &nbsp;
-    </span>
-  );
+const REFRESH_TIME_MILLIS = 500;
+
+// only used to keep track of indices, never filled with anything else
+const rows = Array.from(Array(ROW_COUNT), (_, i) => i);
+export const cols = Array.from(Array(COL_COUNT), (_, i) => i);
+
+type CellProps = {
+  row: number;
+  col: number;
+  color?: string;
+  owner?: string;
+  blinking?: boolean;
+  touched?: boolean;
+};
+
+type Cells = { [cellId: string]: CellProps };
+
+const cellId = (row: number, col: number) => `${row}-${col}`;
+
+// only used for initialization, then we'll use the hook
+const cellsProps: Cells = {};
+
+for (const r of rows) {
+  for (const c of cols) {
+    cellsProps[cellId(r, c)] = { row: r, col: c };
+  }
+}
+
+const Cell = React.memo((props: CellProps) => (
+  <span className={`Cell ${(props.color && `bg-${props.color}`) || ""}`}>&nbsp;</span>
+));
+
+export type GameRef = {
+  addGem: (newGem: CellProps) => any;
+};
+
+const Game = (props: {}, ref: React.Ref<any>) => {
+  const [cells, setCells] = useState(cellsProps);
+
+  useImperativeHandle(ref, () => ({
+    addGem(newGem: CellProps) {
+      setCells({ ...cells, [cellId(newGem.row, newGem.col)]: newGem });
+    },
+  }));
+
+  useInterval(() => {
+    for (const r of rows) {
+      for (const c of cols) {
+        const cell = cells[cellId(r, c)];
+        const { row, col, color, touched } = cell;
+        const cellBelow = cells[cellId(row + 1, col)];
+        if (color && !touched && cellBelow && !cellBelow.color) {
+          cell.color = undefined;
+          cellBelow.color = color;
+          cellBelow.touched = true;
+        }
+      }
+    }
+
+    Object.values(cells).forEach((c) => {
+      c.touched = false;
+    });
+
+    setCells({ ...cells });
+  }, REFRESH_TIME_MILLIS);
 
   const getRow = (r: number) => (
     <div key={`row-${r}`} className="Row">
-      {cols.map((_, c) => getCol(r, c))}
+      {cols.map((_, c) => (
+        <Cell key={cellId(r, c)} {...cells[cellId(r, c)]} />
+      ))}
     </div>
   );
 
   return <div className="Game">{rows.map((_, r) => getRow(r))}</div>;
 };
 
-export default Game;
+export default forwardRef<GameRef, {}>(Game);
