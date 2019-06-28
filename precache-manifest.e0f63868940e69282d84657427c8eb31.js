f24ab995df7488ddfbc010c60bd6e462c87094d5
Michele Bonazza <michele@sketchtogether.com>
2019-06-28 08:45:10 +0200

    Updates

---
+++ precache-manifest.e0f63868940e69282d84657427c8eb31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e933259c8050efe1235c1db72689785",
+    "url": "/gemchain/index.html"
+  },
+  {
+    "revision": "9ea9d91ce8efcb092da3",
+    "url": "/gemchain/static/css/main.222877fa.chunk.css"
+  },
+  {
+    "revision": "63849517d58b8657b1d2",
+    "url": "/gemchain/static/js/2.195357f4.chunk.js"
+  },
+  {
+    "revision": "9ea9d91ce8efcb092da3",
+    "url": "/gemchain/static/js/main.ebcc9ab2.chunk.js"
+  },
+  {
+    "revision": "c8ab424212d79aefa5fa",
+    "url": "/gemchain/static/js/runtime~main.3ce32519.js"
+  }
+]);
